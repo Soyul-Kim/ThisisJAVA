9c7d5d2085dd1e9a07515600d23f763533c0b0e0
Soyul_Kim <todayfcn@gmail.com>
2023-01-26 22:50:49 +0900

    Chap02

---
+++ src/Chap02_variable/CharExample.java
@@ -0,0 +1,20 @@
+package Chap02_variable;
+
+public class CharExample {
+    public static void main(String[] args) {
+        char c1='A'; //문자 저장
+        char c2=65; //유니코드 직접 저장
+
+        char c3='가'; //문자 저장
+        char c4=44032; //유니코드 직접 저장
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c4);
+
+        //char 타입 초기화
+        char c = ' '; //공백 하나를 포함해서 초기화
+        //char c = ''; //컴파일 에러
+    }
+}
---
+++ src/Chap02_variable/FloatDoubleExample.java
@@ -0,0 +1,11 @@
+package Chap02_variable;
+
+public class FloatDoubleExample {
+    public static void main(String[] args) {
+        //정밀도 확인
+        float var1 = 0.1234567890123456789f;
+        double var2 = 0.1234567890123456789;
+        System.out.println("var01: " + var1);
+        System.out.println("var02: " + var2);
+    }
+}
