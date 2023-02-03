1ed15092e36363e2bc6366c248101bc556302cc5
Soyul_Kim <todayfcn@gmail.com>
2023-02-03 16:57:52 +0900

    Chap04 4.4~4.8

---
+++ src/Chap04/FloatCounterExample.java
@@ -0,0 +1,9 @@
+package Chap04;
+
+public class FloatCounterExample {
+    public static void main(String[] args) {
+        for (float x = 0.1f; x <= 1; x += 0.1f) {
+            System.out.println(x);
+        }
+    }
+}
