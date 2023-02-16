7612ee46bc51dc803e0bc55dc6bc26b568d0d1f9
Soyul_Kim <todayfcn@gmail.com>
2023-02-17 00:49:21 +0900

    Chap06 6.12

---
+++ src/Chap06/sec12/hyundai/Car.java
@@ -0,0 +1,11 @@
+package Chap06.sec12.hyundai;
+
+import Chap06.sec12.hankook.SnowTire;
+import Chap06.sec12.kumho.AllSeasonTire;
+
+public class Car {
+    Chap06.sec12.hankook.Tire tire1=new Chap06.sec12.hankook.Tire();
+    Chap06.sec12.kumho.Tire tire2=new Chap06.sec12.kumho.Tire();
+    SnowTire tire3=new SnowTire();
+    AllSeasonTire tire4=new AllSeasonTire();
+}
