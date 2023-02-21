1d50b6212aa2fb1d59e9fdc47680ffd10e1cfcd8
Soyul_Kim <todayfcn@gmail.com>
2023-02-21 23:48:37 +0900

    ch07 7.3

---
+++ src/ch07/sec03/exam01/SmartPhoneExample.java
@@ -0,0 +1,10 @@
+package ch07.sec03.exam01;
+
+public class SmartPhoneExample {
+    public static void main(String[] args) {
+        SmartPhone myPhone=new SmartPhone("Galaxy", "Silver");
+
+        System.out.println(myPhone.model);
+        System.out.println(myPhone.color);
+    }
+}
---
+++ src/ch07/sec03/exam02/SmartPhoneExample.java
@@ -0,0 +1,11 @@
+package ch07.sec03.exam02;
+
+public class SmartPhoneExample {
+    public static void main(String[] args) {
+        SmartPhone myPhone=new SmartPhone("Galaxy", "Silver");
+
+        System.out.println(myPhone.model);
+        System.out.println(myPhone.color);
+
+    }
+}
