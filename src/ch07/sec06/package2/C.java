b207aba21551b7d0afda5609afe1c42dade299b2
Soyul_Kim <todayfcn@gmail.com>
2023-03-03 10:46:14 +0900

    ch07 7.7

--- src/ch07/sec06/package2/C.java
+++ src/ch07/sec06/package2/C.java
@@ -1,7 +1,6 @@
 package ch07.sec06.package2;
 
-import ch07.sec06.package1.A;
-public class C {
+ public class C {
     public void method(){
         //A a.field="value";=new A();
         //a.field="value";
