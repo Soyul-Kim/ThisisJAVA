727bffbddb9dd8c8c9c0f6826684243e2523f3b6
Soyul_Kim <todayfcn@gmail.com>
2023-03-23 00:50:43 +0900

    8.9

---
+++ src/ch08/sec09/InterfaceImpl.java
@@ -0,0 +1,15 @@
+package ch08.sec09;
+
+public class InterfaceImpl implements InterfaceC{
+    public void methodA(){
+        System.out.println("A 실행");
+    }
+
+    public void methodB(){
+        System.out.println("B 실행");
+    }
+
+    public void methodC(){
+        System.out.println("C 실행");
+    }
+}
