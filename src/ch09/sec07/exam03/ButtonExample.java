58d5b15033995508765c4f19f619907ec05722c9
Soyul_Kim <todayfcn@gmail.com>
2023-04-17 07:31:56 +0900

    9.7

---
+++ src/ch09/sec07/exam03/ButtonExample.java
@@ -0,0 +1,9 @@
+package ch09.sec07.exam03;
+
+import ch09.sec06.exam03.Button;
+
+public class ButtonExample {
+    public static void main(String[] args) {
+        Button button=new Button();
+    }
+}
