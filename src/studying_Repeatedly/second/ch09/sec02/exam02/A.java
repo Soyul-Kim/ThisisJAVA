8a0869b8902ea2bbc1379ec3414a6757b8001266
Soyul_Kim <todayfcn@gmail.com>
2023-04-22 00:10:28 +0900

    9.2

--- src/studying_Repeatedly/second/ch09/sec02/exam02/A.java
+++ src/studying_Repeatedly/second/ch09/sec02/exam02/A.java
@@ -6,6 +6,16 @@ public class A {
 
         static int field2=2;
 
+        B(){
+            System.out.println("B 생성자 실행");
+        }
 
+        void method1(){
+            System.out.println("B method1 실행");
+        }
+
+        static void method2(){
+            System.out.println("B method2 실행");
+        }
     }
 }
