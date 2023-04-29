df6f26b8977b4176ebc9a16ee482d4698aa79f7f
Soyul_Kim <todayfcn@gmail.com>
2023-04-29 23:56:52 +0900

    9.4

---
+++ src/studying_Repeatedly/second/ch09/sec04/exam03/A.java
@@ -0,0 +1,14 @@
+package studying_Repeatedly.second.ch09.sec04.exam03;
+
+public class A {
+    public void method1(int arg){
+        int var=1;
+
+        class B{
+            void method2(){
+                System.out.println(arg);
+                System.out.println(var);
+            }
+        }
+    }
+}
